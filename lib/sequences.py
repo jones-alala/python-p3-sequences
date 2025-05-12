63c22f500920ec29e508414cb98183e5c7ead86e
Johnson Alala <jonesalala95@gmail.com>
2025-05-12 03:19:47 +0300

    Sequence

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
-#!/usr/bin/env python3
+# lib/sequences.py
 
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+        return
+
+    fibonacci = [0, 1]
+    while len(fibonacci) < length:
+        next_number = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(next_number)
+
+    print(fibonacci[:length])
